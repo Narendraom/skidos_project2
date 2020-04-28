4ee9aaef5b0d1600de5636ba2882b4c8e29f1c40
USER <user@MacBooks-iMac.local>
2020-04-28 22:23:46 +0530

    Working on COntent Provider Plugin

--- Assets/Plugins/Android/NativeBridge.cs
+++ Assets/Plugins/Android/NativeBridge.cs
@@ -33,16 +33,16 @@ public class NativeBridge
         AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 
         // Retrieve the UnityPlayerActivity object ( a.k.a. the current context )
-        AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+      //  AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
 
         // Retrieve the "Bridge" from our native plugin.
         // ! Notice we define the complete package name.              
         AndroidJavaObject bridge = new AndroidJavaObject("com.naren.sharedsdk.ShareData");
 
         // Setup the parameters we want to send to our native plugin.              
-        object[] parameters = new object[2];
-        parameters[0] = unityActivity;
-        parameters[1] = key;
+        object[] parameters = new object[1];
+        //parameters[0] = unityActivity;
+        parameters[0] = key;
 
         // Call PrintString in bridge, with our parameters.
         return bridge.Call<string>("getPreferenceString", parameters);
